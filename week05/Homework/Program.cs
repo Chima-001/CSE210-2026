622ec65d4ef60bf1a1792512e25bf7f5d87bd509
Chima-001 <cnnaji055@gmail.com>
2026-04-03 03:10:55 +0100

    feat(week05): add Assignment, MathAssignment, and WritingAssignment classes with summary and homework list methods

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment ass1 = new("Chimamkpam Nnaji", "C-Sharp");
+        Console.WriteLine(ass1.GetSummary());
+        Console.WriteLine();
+        
+        MathAssignment ass2 = new("Diego Santoz", "Fractions", "7.4", "11-4");
+        Console.WriteLine(ass2.GetSummary());
+        Console.WriteLine(ass2.GetHomeWorkList());
+        Console.WriteLine();
+
+        WritingAssignment ass3 = new("Abel Camia", "European History", "The Causes of World War II");
+        Console.WriteLine(ass3.GetSummary());
+        Console.WriteLine(ass3.GetWritingInformation());
+
     }
 }
