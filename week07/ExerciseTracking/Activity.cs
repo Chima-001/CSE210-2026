499777cc3e6d11792b198be0a884c06c410a9a2f
Chima-001 <cnnaji055@gmail.com>
2026-04-12 10:28:45 +0100

    Add Running and Swimming classes for activity tracking
    - Implemented Running class to calculate speed, distance, and pace based on distance covered.
    - Implemented Swimming class to calculate speed, distance, and pace based on laps completed.

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,31 @@
+public abstract class Activity
+{
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public int GetMinutes()
+    {
+        return _minutes;
+    }
+
+    public string GetDate()
+    {
+        return _date;
+    }
+
+    public virtual string GetSummary()
+    {
+        return $"{_date} {GetType().Name} ({_minutes} min)- Distance : {GetDistance():0.0} miles, Speed {GetSpeed():0.0} mph, Pace: {GetPace():0.0} min per mile";
+    }
+
+    public abstract double GetDistance();
+    public abstract double GetPace();
+    public abstract double GetSpeed();
+
+}
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<Activity> activities = [];
+
+        activities.Add(new Running("13 Jan 2025", 25, 2.3));
+        activities.Add(new Running("07 Mar 2025", 50, 5.1));
+        activities.Add(new Running("19 Aug 2024", 35, 3.7));
+
+        activities.Add(null);
+
+        activities.Add(new Cycling("02 Feb 2025", 40, 11.5));
+        activities.Add(new Cycling("28 Jun 2024", 55, 8.3));
+        activities.Add(new Cycling("11 Oct 2024", 20, 14.2));
+
+        activities.Add(null);
+
+        activities.Add(new Swimming("15 Apr 2025", 30, 17));
+        activities.Add(new Swimming("03 Jan 2024", 48, 25));
+        activities.Add(new Swimming("22 Jan 2024", 62, 33));
+
+        foreach (Activity activity in activities)
+        {
+            if (activity == null) Console.WriteLine();
+            else
+            {
+                Console.WriteLine(activity.GetSummary());
+            }
+        }
     }
+
 }
